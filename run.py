9d0cd61ae158cf97b6d7a36d275eb129e7a10763
ericyuan <ericyuan@berkeley.edu>
2023-04-14 14:58:44 -0700

    larger scale test

--- run.py
+++ run.py
@@ -15,9 +15,9 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = Diffusion(n_layer=9, n_feat=256, n_atomtype=2)
 
 train_dataset = CustomDataset(X, Z)
-train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=100, shuffle=True, drop_last=True)
+train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=64, shuffle=True, drop_last=True)
 val_dataset = CustomDataset(X, Z)
-val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=100, shuffle=True, drop_last=True)
+val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=64, shuffle=True, drop_last=True)
 
 trainer = Trainer(model, lr=1.0e-3, epoch=1000, device=device)
 res = trainer.train(train_dataloader, val_dataloader)
