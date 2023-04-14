9f30f33ef8dd3450703a3df1f5d575c7a07ceb52
ericyuan <ericyuan@berkeley.edu>
2023-04-14 12:38:02 -0700

    update gpu usage

--- run.py
+++ run.py
@@ -6,18 +6,22 @@ from data import *
 from trainer import *
 from sampler import *
 
-
 X = np.zeros((1000, 2, 3))
 for i in range(1000):
     X[i, 1, 2] = 1
 Z = np.ones((1000, 2, 1))
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
 model = Diffusion()
+model.to(device=device)
 
 train_dataset = CustomDataset(X, Z)
-train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=100, shuffle=True, drop_last=True)
+train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=100, shuffle=True, drop_last=True,
+                                               pin_memory=True, pin_memory_device=device)
 val_dataset = CustomDataset(X, Z)
-val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=100, shuffle=True, drop_last=True)
+val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=100, shuffle=True, drop_last=True,
+                                             pin_memory=True, pin_memory_device=device)
 
 trainer = Trainer(model, lr=1.0e-3, epoch=100)
 res = trainer.train(train_dataloader, val_dataloader)
