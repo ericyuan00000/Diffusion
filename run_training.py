e2d16562acadd1ab6f5e1a60d12412a5cc2c8e7f
ericyuan <ericyuan@berkeley.edu>
2023-04-16 22:08:52 -0700

    Check exploding gradient

--- run_training.py
+++ run_training.py
@@ -17,7 +17,7 @@ val_dataset = CustomDataset(val_data, n_atomtype=10)
 val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=64, shuffle=True, drop_last=True)
 
 trainer = Trainer(model, lr=1.0e-4, n_epoch=1000, save_model=20, save_path='output_qm9', device=device)
-trainer.train(train_dataloader, val_dataloader, verbose=True)
+trainer.train(train_dataloader, val_dataloader, verbose=False)
 
 # train_data = {'R': np.tile(np.array([[0,0,0],[0,0,1]]), [1000, 1, 1]),
 #               'Z': np.tile(np.array([[1],[1]]), [1000, 1, 1])}
