d4fea8c5a5ced42a38953c45f96e9e15a4c29d6d
ericyuan <ericyuan@berkeley.edu>
2023-04-16 23:08:12 -0700

    Check exploding gradient

--- trainer.py
+++ trainer.py
@@ -50,7 +50,7 @@ class Trainer():
                 loss = self.loss_func(pred_epsilon, batch_epsilon)
                 self.optimizer.zero_grad()
                 loss.backward()
-                torch.nn.utils.clip_grad_norm_(self.model.parameters(), max_norm=1.0)
+                torch.nn.utils.clip_grad_norm_(self.model.parameters(), max_norm=1.0, error_if_nonfinite=True)
                 self.optimizer.step()
                 train_loss += loss.detach().cpu().item()/len(train_dataloader)
             
