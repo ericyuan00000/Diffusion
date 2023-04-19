6f98b139cfc8620929aa3ff921c34b84c99d2cb8
ericyuan <ericyuan@berkeley.edu>
2023-04-18 20:06:58 -0700

    Update masking

--- data.py
+++ data.py
@@ -4,14 +4,18 @@ from torch.utils.data import Dataset
 class CustomDataset(Dataset):
     def __init__(self, data, atomtype=[1, 6, 7, 8, 9]):
         self.X = torch.tensor(data['R'], dtype=torch.float)
+        print(self.X.shape)
         n_sample = self.X.shape[0]
         n_atom = self.X.shape[1]
         n_atomtype = len(atomtype)
         self.Z = torch.zeros((n_sample, n_atom, n_atomtype))    # atom types, (n_sample, n_atom, n_atomtype)
+        print(self.Z.shape)
         self.K1 = torch.ones((n_sample, n_atom, 1))    # node masks, (n_sample, n_atom, 1)
         self.K1[data['Z']==0] = 0
+        print(self.K1.shape)
         self.K2 = (self.K1 * self.K1.permute(0, 2, 1)).unsqueeze(3)   # edge masks, (n_sample, n_atom, n_atom, 1)
         self.K2.diagonal(dim1=1, dim2=2).zero_()
+        print(self.K2.shape)
         
 
     def __len__(self):
